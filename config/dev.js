39c914d4ac0d0bdf82f7c5da293cc60874103717
FIGUJ142 <Julian.X.Figueroa.Torres.-ND@disney.com>
2019-05-27 17:57:37 -0500

    Adding testing with Jest & Puppeteer

--- config/dev.js
+++ config/dev.js
@@ -2,6 +2,6 @@ module.exports = {
   googleClientID:
     '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
   googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
-  mongoURI: 'mongodb://julian:Node5@ds049104.mlab.com:49104/blog_dev_',
+  mongoURI: 'mongodb://<dbuser>:<dbpassword>@ds049104.mlab.com:49104/blog_dev_',
   cookieKey: '123123123'
 };
