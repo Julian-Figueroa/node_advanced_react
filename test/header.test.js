223485d9d9ad88ec2f47d375e1b409a5d824f309
FIGUJ142 <Julian.X.Figueroa.Torres.-ND@disney.com>
2019-05-28 15:16:00 -0500

    Adding Proxy to Handle Puppeteer page methods in tests cases with Jest

--- test/header.test.js
+++ test/header.test.js
@@ -1,22 +1,18 @@
-const puppeteer = require('puppeteer');
+const Page = require('./helpers/page');
 
-let browser, page;
+let page;
 
 beforeEach(async () => {
-    browser = await puppeteer.launch({
-        headless: false
-    });
-    page = await browser.newPage();
+    page = await Page.build();
     await page.goto('localhost:3000');
 });
 
 afterEach(async () => {
-    await browser.close();
+    await page.close();
 });
 
 test('Check that the Apps Headers is Blogster', async () => {
-
-    const text = await page.$eval('a.brand-logo', el => el.innerHTML);
+    const text = await page.getContentsOf('a.brand-logo');
 
     expect(text).toEqual('Blogster');
 });
@@ -27,4 +23,12 @@ test('clicking login starts OAuth flow', async () => {
     const url = await page.url();
 
     expect(url).toMatch(/accounts\.google\.com/);
+});
+
+test('When signed in, shows logout button', async () => {
+    await page.login();
+
+    const text = await page.getContentsOf('a[href="/auth/logout"]');
+
+    expect(text).toEqual('Logout');
 });
